39b01ba540e68005267aeff05af48230826b63c5
Andreas Skotadis <skottydeveloper@gmail.com>
2022-01-11 13:28:57 +1100

    Improve code to grocery store mini-project.

--- projects/grocery_store/grocery_store.c
+++ projects/grocery_store/grocery_store.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    int appleQuantity = 23;
-    float applePrice = 1.49;
-    double appleReview = 82.5;
+int main() { 
+    int appleQuantity;
+    double applePrice = 1.49;
+    double appleReview;
     int appleReviewDisplay;
-    const char APPLELOCATION = 'F';
+    const char appleLocation = 'F';
+    int dayOfWeek = 2;
 
-    appleReviewDisplay = (double) appleReview;
+    appleQuantity = 23;
 
-    printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!", applePrice, appleQuantity, APPLELOCATION, appleReviewDisplay);
+    appleReview = 875 / 10;
+    appleReviewDisplay = appleReview;
+
+    dayOfWeek += 2;
+ 
+    if (appleQuantity < 10 && dayOfWeek == 3) {
+        printf("Sale on apples today, today only they are: $%.2f\n", applePrice * .9);
+    }
+    
+    printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!", applePrice, appleQuantity, appleLocation, appleReviewDisplay);
 }
