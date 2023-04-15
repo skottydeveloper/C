703dbde8aaf53c7803c22654cbb5f062ae1015fc
Andreas Skotadis <skottydeveloper@gmail.com>
2023-04-15 18:47:52 +1000

    Refactor code.

--- projects/embedded_c/esp32/buzzer/main/buzzer.c
+++ projects/embedded_c/esp32/buzzer/main/buzzer.c
@@ -1,3 +1,7 @@
+/* This program demonstrates the use of FreeRTOS tasks and mutexes to control access to a buzzer.
+   Two tasks with different priorities are created to generate a 1 kHz square wave signal on the buzzer.
+   A mutex is used to ensure that only one task has access to the buzzer at a time. */
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
@@ -6,10 +10,18 @@
 
 #define BUZZER_PIN 32 // GPI32
 
+// Define constants for task properties.
+#define STACK_SIZE 2048
+#define TASK1_PRIORITY 2
+#define TASK2_PRIORITY 1
+#define TASK1_CORE 0
+#define TASK2_CORE 1
+
 // Declare a semaphore handle for the mutex.
 SemaphoreHandle_t buzzer_mutex;
 
-// The buzzer_task function is designed to be run in a separate FreeRTOS task.
+/* The buzzer_task function generates a 1 kHz square wave signal on the buzzer.
+   It runs in a separate FreeRTOS task and uses a mutex to ensure exclusive access to the buzzer. */
 void buzzer_task(void *arg) {
     // Get the task number from the argument passed to the function.
     int task_num = (int) arg;
@@ -17,37 +29,26 @@ void buzzer_task(void *arg) {
     // Calculate the delay period based on the task's number.
     int delay_period = (task_num) * 1000 / portTICK_PERIOD_MS;
 
-    // Declare a variable to store the "esp_rom_delay" value.
-    int esp_rom_delay;
+    // Determine the "esp_rom_delay" value based on the task number.
+    int esp_rom_delay = (task_num > 1) ? 250 : 125;
 
-    // Set the "esp_rom_delay" value based on the task number.
-    if (task_num > 1) {
-        esp_rom_delay = 250;
-    } else {
-        esp_rom_delay = 125;
-    }
+    // Array to store the pin levels (high and low).
+    int pin_levels[] = {1, 0};
 
     // Main loop of the task.
     while (1) {
-        /* Try to take the mutex. This call will block (wait) until the mutex is available.
-           "portMAX_DELAY" means it will wait indefinitely for the mutex to be available. */
+        // Attempt to take the mutex, waiting indefinitely until it's available.
         if (xSemaphoreTake(buzzer_mutex, portMAX_DELAY) == pdTRUE) {
-            // If the mutex is successfully taken, print the task number that got access to the buzzer.
-            printf("Task %d got access to buzzer.\n", task_num);
-
             // Generate square wave signal at 1 kHz.
             for (int i = 0; i < 1000; i++) {
-                // Set the buzzer pin to high (1).
-                gpio_set_level(BUZZER_PIN, 1);
+                // Loop through the pin levels array (high and low)
+                for (int j = 0; j < sizeof(pin_levels) / sizeof(pin_levels[0]); j++) {
+                    // Set the buzzer pin to the current level from the pin_levels array
+                    gpio_set_level(BUZZER_PIN, pin_levels[j]);
 
-                // Delay for the specified "esp_rom_delay" value.
-                esp_rom_delay_us(esp_rom_delay);
-
-                // Set the buzzer pin to low (0).
-                gpio_set_level(BUZZER_PIN, 0);
-
-                // Delay for the specified "esp_rom_delay" value.
-                esp_rom_delay_us(esp_rom_delay);
+                    // Delay for the specified "esp_rom_delay" value.
+                    esp_rom_delay_us(esp_rom_delay);
+                }
             }
 
             // Release the mutex after using the buzzer, allowing other tasks to take control of it.
@@ -66,12 +67,9 @@ void app_main(void) {
     // Create the mutex for buzzer access. This is a binary semaphore used for mutual exclusion.
     buzzer_mutex = xSemaphoreCreateMutex();
 
-    /* Create two tasks with different priorities, each pinned to a different core. You can assign
-    higher priorities to tasks running on Core 0 and lower priorities to tasks running on Core 1. */
+    // Create and pin Task 1 to Core 0 with a higher priority.
+    xTaskCreatePinnedToCore(buzzer_task, "Buzzer Task 1", STACK_SIZE, (void *) 1, TASK1_PRIORITY, NULL, TASK1_CORE);
 
-    // Task 1 is pinned to Core 0 with a higher priority (2).
-    xTaskCreatePinnedToCore(buzzer_task, "Buzzer Task 1", 2048, (void *) 1, 2, NULL, 0);
-
-    // Task 2 is pinned to Core 1 with a lower priority (1).
-    xTaskCreatePinnedToCore(buzzer_task, "Buzzer Task 2", 2048, (void *) 2, 1, NULL, 1);
+    // Create and pin Task 2 to Core 1 with a lower priority.
+    xTaskCreatePinnedToCore(buzzer_task, "Buzzer Task 2", STACK_SIZE, (void *) 2, TASK2_PRIORITY, NULL, TASK2_CORE);
 }
